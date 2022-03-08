4c6889cd42416719eb521d2b14cd0b9fbcf82cfb
Farhan910 <farhanfaruk221@gmail.comm>
2022-03-09 02:04:59 +0600

    last

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('hey');
+const allName = () => {
+  const num = prompt('hey warning')
+  const parsing =parseInt(num)
+  const numb = parsing + 200;
+  alert(numb)
+
+  const link = confirm("you want to see the link ?");
+  if (link == true) {
+    console.log(5500);
+  }
+
+
+
+};
+
+// const name = 'Hero';const age = 34;
+// const person = {name, age}; console.log(person);
+
+// const person = { name : ”Babe” }
+// console.log(person.name);
+
+
+const adventurer = { name: 'Alice',cat: {name: 'Lucy'} };
+const ratName = adventurer?.rat?.name;
+console.log(ratName);
+
+let person =null;
+console.log(person ? person : "person is null");
+
+
+const obj = {a:1};
+console.log(Object.keys(obj).length===0);
